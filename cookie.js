9233efb7857bb1bbb6dd2ce6130d8c46b0cfb4b7
RedsXi <90264141+RedsXi@users.noreply.github.com>
2022-08-28 07:49:04 +0800

    Javascript for Cookies

---
+++ cookie.js
@@ -0,0 +1,25 @@
+// wrote in 2022/8/28
+
+function setCookie(cname,cvalue,exdays)
+{
+  var d = new Date();
+  d.setTime(d.getTime()+(exdays*24*60*60*1000));
+  var expires = "expires="+d.toGMTString();
+  document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname)
+{
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) 
+  {
+    var c = ca[i].trim();
+    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  }
+  return undefined;
+}
+
+function checkCookie(cname) {
+  return (getCookie(cname) != undefined);
+}
